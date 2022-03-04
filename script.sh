cf257de462e80cb071e2e6d7bfb8d7179603cd16
din <62234787+baharuddinzulkifli@users.noreply.github.com>
2022-03-04 22:41:18 +0800

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,9 +1,15 @@
-USERNAME="" #your username
-PASSWD="" #your password
-CMD="df /dev/sda2 --output=pcent"
+user="" #your username
+passwd="" #your password
+host=("host ip" "host ip")
+cmd="df /dev/sda1 --output=pcent"
+for i in ${!host[*]} ; do
+	ping -c 2 ${host[i]} > /dev/null
+    		if [ $? = 0 ]; then
+    		echo "Splunk Indexer: "  ${host[i]} "Alive"
+    		else
+    		echo "Splunk Indexer: " ${host[i]} "down" 
+    		fi
+	echo ${host[i]}
+	sshpass -p $passwd ssh  $user@${host[i]} $cmd
+done
 
-cat host.txt | while read host; do
-	echo "Host: $host"
-	sshpass -p $PASSWD ssh -o StrictHostKeyChecking=no  $USERNAME@$host $CMD 	
-
-done 
